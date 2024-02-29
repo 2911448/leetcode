4cef5769819f38cc73e801a800401c142cbf5b0d
yanxin <yanxin@deepway.ai>
2024-02-29 10:07:08 +0800

    链表排序

---
+++ hot/148.go
@@ -0,0 +1,70 @@
+package hot
+
+import "leetcode/common"
+
+func SortList(head *common.ListNode) *common.ListNode {
+	if head == nil {
+		return head
+	}
+
+	length := 0
+	for node := head; node != nil; node = node.Next {
+		length++
+	}
+
+	dummyHead := &common.ListNode{Next: head}
+	for subLength := 1; subLength < length; subLength <<= 1 {
+		prev, cur := dummyHead, dummyHead.Next
+		for cur != nil {
+			head1 := cur
+			for i := 1; i < subLength && cur.Next != nil; i++ {
+				cur = cur.Next
+			}
+
+			head2 := cur.Next
+			cur.Next = nil
+			cur = head2
+			for i := 1; i < subLength && cur != nil && cur.Next != nil; i++ {
+				cur = cur.Next
+			}
+
+			var next *common.ListNode
+			if cur != nil {
+				next = cur.Next
+				cur.Next = nil
+			}
+
+			prev.Next = merge(head1, head2)
+
+			for prev.Next != nil {
+				prev = prev.Next
+			}
+			cur = next
+		}
+	}
+	return dummyHead.Next
+
+}
+
+// 组合两个有序链表
+func merge(list1 *common.ListNode, list2 *common.ListNode) *common.ListNode {
+	dummy := &common.ListNode{}
+	current, l1, l2 := dummy, list1, list2
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			current.Next = l1
+			l1 = l1.Next
+		} else {
+			current.Next = l2
+			l2 = l2.Next
+		}
+		current = current.Next
+	}
+	if l1 != nil {
+		current.Next = l1
+	}
+	if l2 != nil {
+		current.Next = l2
+	}
+	return dummy.Next
+}
